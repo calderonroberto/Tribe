206a15b80da88107587b0fbd1e90aa80772b5167
Roberto Calderon <roberto@robertocalderon.ca>
2016-02-21 12:14:08 -0800

    fixed revert

--- api/controllers/TribeController.js
+++ api/controllers/TribeController.js
@@ -7,22 +7,9 @@
 
 module.exports = {
 	new: function(req,res){
-		if ( !req.isAuthenticated() ) return res.forbidden();
+	  if ( !req.isAuthenticated() ) return res.forbidden();
 		res.view({user: req.user},'tribe/new')
 	},
-
-
-	//Upload file to Gridfs service
-	/*uploadFile: function (req, res) {
-	 req.file('avatar').upload({
-	 adapter: require('skipper-gridfs'),
-	 uri: 'mongodb://[username:password@]host1[:port1][/[database[.bucket]]'
-	 }, function (err, filesUploaded) {
-	 if (err) return res.negotiate(err);
-	 return res.ok();
-	 });
-	 },*/
-
 
 	upload: function  (req, res) {
 
@@ -30,32 +17,30 @@ module.exports = {
 		var description = req.param('description');
 		var members = req.param('members');
 
-		req.file('photo').upload(
+    req.file('photo').upload(
+		//TODO: currently uploads are stored in ''/assets/images/photos/'.
+		// This is very inefficinent, you should use something like S3 or another file storage service.
+		{
+			dirname: sails.config.appPath+'/assets/public/tribes/'
+		},
+		function (err, files) {
 
-			//TODO: currently uploads are stored in ''/assets/images/photos/'.
-			// This is very inefficinent, you should use something like S3 or another file storage service.
-			{
-				dirname: sails.config.appPath+'/assets/public/tribes/'
-			},
-			function (err, files) {
+      if (err)
+        return res.serverError(err);
 
-				if (err)
-					return res.serverError(err);
-
-				if (files.length > 0) {
-					var url = files[0].fd.substring(files[0].fd.lastIndexOf('/')+1,files[0].fd.length);
-					Tribe.create({
-						image_url:url,
-						description: description,
-						name: name,
-						members: members
-					}).exec(function createCB(err, created){
-						return res.json(created);
-					});
-				} else {
-					return res.json({})
-				}
-
+			var url = files[0].fd.substring(files[0].fd.lastIndexOf('/')+1,files[0].fd.length);
+			Tribe.create({
+				image_url:url,
+				description: description,
+				name: name,
+				members: members
+			}).exec(function createCB(err, created){
+				return res.json(created);
 			});
-	}
+    });
+  }
+
+
+
+>>>>>>> parent of b1fa045... First TribesFrontEnd
 };
